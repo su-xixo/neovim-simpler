df06d9114862d4aee2a3e613f44efa554642c4f7
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-11 13:59:01 +0530

    LSP: lua_ls lsp added

--- init.lua
+++ init.lua
@@ -323,6 +323,26 @@ require("lazy").setup({
 })
 -- }}}
 
+-- LSP {{{
+vim.lsp.config["lua_ls"] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+    }
+  }
+}
+
+
+vim.lsp.enable({ "lua_ls", "bashls" })
+-- }}}
 
 -- KEYMAPS {{{
 vim.g.mapleader = ' '
